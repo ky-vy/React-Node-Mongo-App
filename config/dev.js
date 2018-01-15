dd0e67c47ccc6d11f117f916e4ff9cded7f0323e
Kyryll Vynokurov <cy.vinokurov@gmail.com>
2018-01-15 15:40:43 +0200

    Showing dev keys for proper dev testing

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID:
+    '983729877959-7jjmqbh69vsmhr30plm1lon1qjv65aap.apps.googleusercontent.com',
+  googleClientSecret: '3IdqG8B05nrD7wInA906cueR',
+  mongoURI: 'mongodb://ky-vy:password@ds121336.mlab.com:21336/emaily',
+  cookieKey: 'Whiteboards are remarkable',
+  stripePublishableKey: 'pk_test_jlGoV37WTs9gzZhnJL1vVDxw',
+  stripeSecretKey: 'sk_test_E0i1DD4VeQqMvTSVlUJu86vb',
+  sendGridKey:
+    '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',
+  redirectDomain: 'http://localhost:3000'
+}
